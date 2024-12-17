42aef215af376816c9a68019560447371fd47b28
CarlosITPFP <148757975+CarlosITPFP@users.noreply.github.com>
2024-12-17 14:44:29 +0100

    Estetica del Login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            checkBox1 = new CheckBox();
+            linkLabel1 = new LinkLabel();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(458, 319);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 43);
+            button1.TabIndex = 0;
+            button1.Text = "Iniciar Sesion";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(589, 319);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 43);
+            button2.TabIndex = 1;
+            button2.Text = "Reiniciar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(571, 227);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(130, 19);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "Mostrar Contraseña";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(578, 374);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(123, 15);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Recuperar Contraseña";
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 13F);
+            label1.Location = new Point(314, 95);
+            label1.Name = "label1";
+            label1.Size = new Size(136, 43);
+            label1.TabIndex = 4;
+            label1.Text = "Usuario";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.Location = new Point(314, 173);
+            label2.Name = "label2";
+            label2.Size = new Size(145, 48);
+            label2.TabIndex = 5;
+            label2.Text = "Contraseña";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(532, 95);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(169, 43);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(532, 181);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(169, 40);
+            textBox2.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Usuario;
+            pictureBox1.Location = new Point(50, 78);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(149, 143);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(linkLabel1);
+            Controls.Add(checkBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private CheckBox checkBox1;
+        private LinkLabel linkLabel1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
     }
 }
